59bd0494f86316a6192f4ded18465337009654e7
Sergio <sergiomsrs@gmail.com>
2026-06-12 20:42:04 +0200

    Initial commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>AppRecetas</h1>
+        </div>
+    );
+}
---
+++ src/router/router.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RecipesPage from "../pages/RecipesPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+export const router = createBrowserRouter([
+    {
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/login",
+                element: <LoginPage />,
+            },
+            {
+                path: "/recipes",
+                element: <RecipesPage />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFoundPage />,
+    },
+]);
