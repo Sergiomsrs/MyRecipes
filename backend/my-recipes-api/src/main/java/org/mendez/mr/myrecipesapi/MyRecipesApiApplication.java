2134539f64fdd4ac4116bfa5c7bf748db1917f0d
Sergio <sergiomsrs@gmail.com>
2026-07-23 19:47:30 +0200

    chore: initialize Spring Boot project

---
+++ backend/my-recipes-api/src/main/java/org/mendez/mr/myrecipesapi/MyRecipesApiApplication.java
@@ -0,0 +1,13 @@
+package org.mendez.mr.myrecipesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRecipesApiApplication.class, args);
+    }
+
+}
---
+++ backend/my-recipes-api/src/test/java/org/mendez/mr/myrecipesapi/MyRecipesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mendez.mr.myrecipesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
